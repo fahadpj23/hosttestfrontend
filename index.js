5e9d3f10eac36355b989a05fb55082d6d5aea872
fahadpj23 <fahadpj23@gmail.com>
2022-06-25 13:36:52 +0530

    image add

--- index.js
+++ index.js
@@ -63,17 +63,17 @@ app.get("/userlogin",function(req,res)
   
 app.post("/imageAdd",parseUrlencoded,(req,res)=>{
     
-    // let file= req.files.image
+    let file= req.files.image
                 
     // file && file.mv(`products/images/${Math.round(new Date().getTime()/1000)}${file.name}`)
-    // bannerimageAdd=`insert into image (productImage) values ( '${Math.round(new Date().getTime()/1000)}${file.name}')`
-    // con.query(bannerimageAdd,(err,result)=>{
-    //     if(err) throw (err)
-    //     else
+    bannerimageAdd=`insert into image (productImage) values ( '${Math.round(new Date().getTime()/1000)}${file.name}')`
+    con.query(bannerimageAdd,(err,result)=>{
+        if(err) throw (err)
+        else
            
                 res.json({"success":"image added successfully"})
             
-    // })
+    })
 })
 
 
