89d4b4d12fb4e5ed311fd0ffcf623e74c86816e0
fahadpj23 <fahadpj23@gmail.com>
2022-06-25 12:14:00 +0530

    image add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var jsonParser=bodyParser.json();
 var parseUrlencoded = bodyParser.urlencoded({ extended: true });
 app.use(express.static('public')); 
 app.use('products/images', express.static('images')); 
-app.use(express.static(path.join(__dirname, 'products')))
+// app.use(express.static(path.join(__dirname, 'products')))
 // app.use(express.static('public')); 
 
 const con=require('./database')
