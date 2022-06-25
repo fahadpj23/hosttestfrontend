f330b3e9883be976af3509f1104504b297ae1808
fahadpj23 <fahadpj23@gmail.com>
2022-06-25 13:19:23 +0530

    image add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express=require("express");
 var app=express();
 var cors=require("cors");
 const upload=require('express-fileupload')
+const path = require('path')
 app.use(upload())
 app.use(cors());
 
@@ -11,21 +12,14 @@ var parseUrlencoded = bodyParser.urlencoded({ extended: true });
 app.use(express.static('public')); 
 app.use('products/images', express.static('images'));
 app.use(express.static(path.join(__dirname, 'products')))
-// app.use('products/images', express.static('images')); 
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-// app.use(express.static('public')); 
+
+
 
 const con=require('./database')
-// app.use(express.static(path.join(__dirname, 'products')))
+
 app.get("/fake",function(req,res)
  {
-    // searchqr=`SELECT name,image,mrp,price FROM headset`
-    // con.query(searchqr,(err,result,fields)=>{
-
-    //   if(err) throw(err);
-    //   res.json( [result] )
-      
-//   }) 
+   
 res.json({"dsd":"dsdsvggcgc"})
   
 })
